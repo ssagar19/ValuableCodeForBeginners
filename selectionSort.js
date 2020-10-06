eb46a5900149f8c97db4114e8a0fee65297ca114
ShubhamSagar <55043608+ssagar19@users.noreply.github.com>
2020-10-07 00:42:24 +0530

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,14 @@
+function selectionSort(arr){
+
+for(var i=0; i<arr.length;i++){
+var lowest = i;
+for(var j=i+1;j<arr.length;j++){
+if(arr[j]<arr[lowest]){   lowest = j;}
+}
+var temp= arr[i];
+arr[i] = arr[lowest];
+arr[lowest] = temp;
+}
+return arr;
+
+}
